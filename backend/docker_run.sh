ec8e8d08f3f57a4bd409c7606fc0b9b1ee9dcc34
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-01-25 12:46:08 +0000

    Frontend started

--- backend/docker_run.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm --gpus all\
- -v hf_cache:/root/.cache/huggingface\
- -v tts_cache:/root/.local/share/tts\
- -v processed_audio:/app/sounds\
- -v audio_results:/app/ai-audio\
- -p 8000:8000 \
- speechagent-backend
