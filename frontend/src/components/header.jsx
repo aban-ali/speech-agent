7e2bc655fc3a308ba733f441ea88207f738af63b
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-01-25 12:50:33 +0000

    Frontend almost done

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+
+
+export function Header() {
+  return (
+    <>
+        <h1 style={{ marginBottom: "6px" }}>🎭 Voices in Your Head</h1>
+        <p style={{ margin: "auto 0", color: "#444", marginBottom: 20 }}>
+            Speak once → multiple AI personalities respond → mixed audio output.
+        </p>
+    </>
+    );
+}
