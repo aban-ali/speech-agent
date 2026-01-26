431313dd94dd9ac22ff48b548cbc4efe845e0519
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-01-26 16:20:37 +0000

    Project Completed

--- backend/llm/model.py
+++ backend/llm/model.py
@@ -1,7 +1,19 @@
 from llama_cpp import Llama
+import os
+from huggingface_hub import hf_hub_download
+
+if not os.path.exists("/root/.cache/huggingface/gemma-3-1b-it-Q8_0.gguf"):
+    print("Downloading model...")
+    path = hf_hub_download(
+        repo_id="ggml-org/gemma-3-1b-it-GGUF",
+        filename="gemma-3-1b-it-Q8_0.gguf",
+        local_dir="/root/.cache/huggingface/",
+        local_dir_use_symlinks=False
+    )
+    print(f"Model downloaded to {path}")
 
 llm = Llama(
-    model_path="/root/.cache/huggingface/gemma-3-1b-it-Q4_0.gguf",
+    model_path="/root/.cache/huggingface/gemma-3-1b-it-Q8_0.gguf",
     n_ctx = 512,
     n_gpu_layers = -1,
     n_threads = 8,
